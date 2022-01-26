890b615478acad32ff5c25c69bf141607dfcefd7
shashank <shashank.mishra@betterworks.com>
2022-01-26 16:41:22 +0530

    Refactoring urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,8 @@ from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,
 from . import views
 
 urlpatterns = [
-    #path('', views.home, name='blog-home'),
     path('', PostListView.as_view(), name='blog-home'),
-     path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
--- django_project/urls.py
+++ django_project/urls.py
@@ -14,44 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('register/', user_views.register, name='register'),
-    path('profile/<str:username>/', user_views.profile, name='profile'),
-    path('login/', user_views.Logincustom.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/',
-         auth_views.PasswordResetView.as_view(
-             template_name='users/password_reset.html'
-         ),
-         name='password_reset'),
-    path('password-reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='users/password_reset_done.html'
-         ),
-         name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='users/password_reset_confirm.html'
-         ),
-         name='password_reset_confirm'),
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='users/password_reset_complete.html'
-         ),
-         name='password_reset_complete'),
-    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
-    path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
-    path('', include('blog.urls')),
-    path('weather/', user_views.weather, name='weather'),
-    path('update-weather/', user_views.WeatherUpdate.as_view(), name='update_weather'),
+    path('', include('users.urls')),
 ]
 
 
